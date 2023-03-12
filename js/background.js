012ecd1576eb37bd0ee56b43c4f8952de7917faa
sckcs15 <100522412+sckcs15@users.noreply.github.com>
2023-03-12 20:45:19 +0900

    update css

--- js/background.js
+++ js/background.js
@@ -9,4 +9,5 @@ const chosenImage =  images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+// bgImage.style ="width:100px","height:100px";
 document.body.appendChild(bgImage);
